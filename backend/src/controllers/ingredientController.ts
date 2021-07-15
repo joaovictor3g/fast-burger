7bd7fd97abe1b0015ea21e10c9139095a7864811
emilianots <emiliano@alu.ufc.br>
2021-07-15 15:14:24 -0300

    adds request controller
    D

--- backend/src/controllers/ingredientController.ts
+++ backend/src/controllers/ingredientController.ts
@@ -33,22 +33,99 @@ export const igredientController = {
   },
 
   async getByID(req: Request, res: Response) {
-    const { id } = req.query;
+    const { id } = req.params;
 
-    if(!id){
+    if (!id) {
       return res
-          .status(400)
-          .json({errorMessage: "Id de ingrediente inválido."});
+        .status(400)
+        .json({ success: false, message: "Id de ingrediente inválido." });
     }
 
     try {
       const data = await connection("ingredients")
         .select("*")
-        .from('ingredients')
-        .where("ingredient_id", id as string);
-        console.log(data)
+        .from("ingredients")
+        .where("ingredient_id", id)
+        .first();
 
-      res.status(200).json({ data: data[0] });
+      if (!data) {
+        return res
+          .status(404)
+          .json({ success: false, message: "Ingrediente não encontrado." });
+      }
+
+      res.status(200).json({ data });
+    } catch (err) {
+      return new Error(err);
+    }
+  },
+
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    try {
+      const storedIngredientId = await connection("ingredients")
+        .select("ingredient_id")
+        .where("ingredient_id", id)
+        .first();
+
+      if (!storedIngredientId) {
+        return res
+          .status(404)
+          .json({ success: false, message: "Ingrediente não encontrado." });
+      }
+
+      await connection("ingredients").delete().where("ingredient_id", id);
+
+      res
+        .status(200)
+        .json({ success: true, message: "Ingrediente removido com sucesso." });
+    } catch (err) {
+      return new Error(err);
+    }
+  },
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { name, amount, type } = req.body;
+
+    if (!name) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Nome do ingrediente inválido." });
+    }
+    if (!amount) {
+      return res.status(400).json({
+        success: false,
+        message: "Quantidade do ingrediente inválido.",
+      });
+    }
+    if (!type) {
+      return res.status(400).json({
+        success: false,
+        message: "Tipo do ingrediente inválido.",
+      });
+    }
+    try {
+      const storedIngredientId = await connection("ingredients")
+        .select("ingredient_id")
+        .where("ingredient_id", id)
+        .first();
+
+      if (!storedIngredientId) {
+        return res
+          .status(404)
+          .json({ success: false, message: "Ingrediente não encontrado." });
+      }
+
+      await connection("ingredients")
+        .update({ name, amount, type })
+        .where("ingredient_id", id);
+
+      res.status(200).json({
+        success: true,
+        message: "Ingrediente atualizado com sucesso.",
+      });
     } catch (err) {
       return new Error(err);
     }
