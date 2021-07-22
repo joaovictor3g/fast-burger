f04abfea43a6b1ad3b31bc25fcd29ea32c223b3f
João Victor <jvdias79797@gmail.com>
2021-07-22 19:08:05 -0300

    feat: include ingredient data

---
+++ backend/src/database/seeds/create_ingredient.ts
@@ -0,0 +1,65 @@
+import { Knex } from 'knex';
+
+const ingredients = [
+  {
+    name: "Pão brioche",
+    description: "Uma descrição do pão",
+    ingredient_id: 1,
+    amount: 2,
+    price: 2.5,
+    type: "Pão",
+  },
+  {
+    name: "Pão baguette",
+    description: "Uma descrição do pão baguette",
+    ingredient_id: 2,
+    amount: 4,
+    price: 4.5,
+    type: "Pão",
+  },
+  {
+    name: "Hamburger bovino",
+    description: "Uma descrição do hamburger bovino",
+    ingredient_id: 3,
+    amount: 2,
+    price: 2.5,
+    type: "Carne",
+  },
+  {
+    name: "Hamburger suíno",
+    description: "Uma descrição do hamburger suíno",
+    ingredient_id: 4,
+    amount: 4,
+    price: 4.5,
+    type: "Carne",
+  },
+  {
+    name: "Maionese",
+    description: "Uma descrição do hamburger bovino",
+    ingredient_id: 5,
+    amount: 2,
+    price: 2.5,
+    type: "Carne",
+  },
+  {
+    name: "Ketchup",
+    description: "Uma descrição do hamburger suíno",
+    ingredient_id: 6,
+    amount: 4,
+    price: 4.5,
+    type: "Carne",
+  },
+  {
+    name: "Mostarda",
+    description: "Uma descrição do hamburger suíno",
+    ingredient_id: 7,
+    amount: 4,
+    price: 4.5,
+    type: "Carne",
+  },
+];
+
+
+export async function seed(knex: Knex) {
+  await knex('ingredients').insert(ingredients)
+}
